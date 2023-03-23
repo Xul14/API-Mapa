e398fb9d7897fc1b8260234d4f33019006d2ee7b
Julia <juliasoaresdealmeidaj@gmail.com>
2023-03-23 15:02:41 -0300

    Atualizando

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const preencherDados = async(sigla) => {
     document.getElementById('nomeCapital').innerText = preencherHeader.capital
     document.getElementById('nomeRegiao').innerText = preencherHeader.regiao
 
+//   const preencherListaCidades = await preencherLista(sigla)
+
+//     const ul = document.getElementById('nome-cidades')
+
+//     preencherListaCidades.cidades.forEach(function (cidade) {
+//         const cidades = document.createElement('li')
+//         cidades.textContent = cidade
+//         ul.append(cidades)
+//     })
+    
+    const preencherListaCidades = await preencherLista(sigla)
+    document.getElementById('nome-cidades').innerHTML = preencherListaCidades.cidades
+
 }
 
 const preencherDadosHeader = async(sigla) => {
@@ -17,10 +30,20 @@ const preencherDadosHeader = async(sigla) => {
     const estado = await response.json()
 
     return {
-        sigla: estado.sigla,
-        nome: estado.nome,
+        sigla: estado.uf,
+        nome: estado.descricao,
         capital: estado.capital,
         regiao: estado.regiao
+    }
+}
+
+const preencherLista = async(sigla) => {
+    const url = `http://localhost:8080/v1/senai/cidades/${sigla}`
+    const response = await fetch(url)
+    const estado = await response.json()
+
+    return {
+        cidades: estado.cidades
     }
 }
 
